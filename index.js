8adf7b031c83b5e7d68a5a8483833df65915a463
yunzaifei <zyunfei86@hotmail.com>
2022-03-18 10:47:17 +0800

    custom path aliases with typescript

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-/**
- * @format
+/*
+ * @Author: zhao yunfei
+ * @Date: 2022-03-18 09:51:35
+ * @LastEditors: zhao yunfei
+ * @LastEditTime: 2022-03-18 10:17:26
+ * @Description: 入口文件
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
